a9e99a36c14afa87fea93f7dd52f23b82a4566b3
jjasonzhang <jjasonzhang@tencent.com>
2020-07-16 23:52:49 +0800

    first commit

---
+++ nginx-php/www/config/config_ucenter.php
@@ -0,0 +1,20 @@
+<?php
+
+
+define('UC_CONNECT', 'mysql');
+
+define('UC_DBHOST', '127.0.0.1');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', 'root');
+define('UC_DBNAME', 'ultrax');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`ultrax`.pre_ucenter_');
+define('UC_DBCONNECT', 0);
+
+define('UC_CHARSET', 'utf-8');
+define('UC_KEY', 'd2Wf2dM6T7Ucp5VaX9Q5PdSf29H8G1u4e3hbr7p2qaSdn89clco7ybu34eOcjbk3');
+define('UC_API', 'http://demo-discuzx1.com/uc_server');
+define('UC_APPID', '1');
+define('UC_IP', '');
+define('UC_PPP', 20);
+?>
---
+++ nginx-php/www/data/sysdata/cache_pluginsetting.php
@@ -0,0 +1,7 @@
+<?php
+//Discuz! cache file, DO NOT modify me!
+//Identify: 5e1d0159dfa119c3aec67ec4f0366d6a
+
+$pluginsetting = array (
+);
+?>
