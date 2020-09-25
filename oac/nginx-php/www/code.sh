109767fe908fae4e5c5de5d8061bf898a45239e9
jjasonzhang0215 <jjasonzhang@tencent.com>
2020-09-25 14:58:15 +0800

    test

---
+++ oac/nginx-php/www/code.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+rm -rf ./code.tar.gz
+mkdir code
+cp -r ~/programs/qrain-app/tencentcloud-adapter-oac/. ./code
+cd ./code
+tar -zcvf code.tar.gz ./*
+mv ./code.tar.gz ../
+cd ../
+rm -rf ./code
